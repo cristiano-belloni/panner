2cdbca6de70eee183fa51e6562a715d02e84089a
janesconference <janesconference@bitterspring.net>
2013-10-30 22:32:52 +0000

    Issue #3 - now panner works with resized canvas

--- panner.js
+++ panner.js
@@ -18,6 +18,61 @@ define(['require'], function(require) {
   
     var pluginFunction = function(args, resources) {
 
+        var getEventPosition = function (e, obj) {
+
+            var stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(obj, null).paddingLeft, 10) || 0;
+            var stylePaddingTop = parseInt(document.defaultView.getComputedStyle(obj, null).paddingTop, 10) || 0;
+            var styleBorderLeft = parseInt(document.defaultView.getComputedStyle(obj, null).borderLeftWidth, 10) || 0;
+            var styleBorderTop = parseInt(document.defaultView.getComputedStyle(obj, null).borderTopWidth, 10) || 0;
+            var html = document.body.parentNode;
+            var htmlTop = html.offsetTop;
+            var htmlLeft = html.offsetLeft;
+
+
+            var element = obj,
+                offsetX = 0,
+                offsetY = 0,
+                mx, my;
+
+            // Compute the total offset
+            if (typeof element.offsetParent !== 'undefined') {
+                do {
+                    offsetX += element.offsetLeft;
+                    offsetY += element.offsetTop;
+                } while ((element = element.offsetParent));
+            }
+
+            // Add padding and border style widths to offset
+            // Also add the <html> offsets in case there's a position:fixed bar
+            offsetX += stylePaddingLeft + styleBorderLeft + htmlLeft;
+            offsetY += stylePaddingTop + styleBorderTop + htmlTop;
+
+            mx = e.pageX - offsetX;
+            my = e.pageY - offsetY;
+
+            // this returns in element's css value, without borders
+            var cssWidth = parseInt(document.defaultView.getComputedStyle(obj, null).getPropertyValue("width"), 10) || 0;
+            var cssHeight = parseInt(document.defaultView.getComputedStyle(obj, null).getPropertyValue("height"), 10) || 0;
+
+            //var cssWidth  = obj.offsetWidth;
+            //var cssHeight = obj.offsetHeight;
+
+            var attrWidth = obj.getAttribute("width");
+            var attrHeight = obj.getAttribute("height");
+            var widthScale = attrWidth / cssWidth;
+            var heightScale = attrHeight / cssHeight;
+            //console.log ('*** SCALE', widthScale, heightScale);
+
+            mx *= widthScale;
+            my *= heightScale;
+
+            // We return a simple javascript object (a hash) with x and y defined
+            return {
+                x: mx,
+                y: my
+            };
+        };
+
 
         // Draws a canvas and tracks mouse click/drags on the canvas.
         function Field(canvas) {
@@ -105,7 +160,8 @@ define(['require'], function(require) {
         Field.prototype.handleMouseMove = function(e) {
             if (this.isMouseInside && this.isMouseDown) {
                 // Update the position.
-                this.point = {x: e.offsetX, y: e.offsetY};
+                var p = getEventPosition (e, this.canvas);
+                this.point = {x: p.x, y: p.y};
                 // Re-render the canvas.
                 this.render();
                 // Callback.
